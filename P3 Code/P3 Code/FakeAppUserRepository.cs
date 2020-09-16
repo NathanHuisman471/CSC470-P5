e4d91c928a84ed17ff8697ddbfb6b20c8c1c9cda
Marie Sallman <Marie.Sallman@trojans.dsu.edu>
2020-09-16 13:29:47 -0500

    Renamed FakeEmployeeRepository and IEmployeeRepository to their appropriate names and adjusted the contents of both to reflect the names/variables/code outlined in the P3 document.

---
+++ P3 Code/P3 Code/FakeAppUserRepository.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections.Generic;
+namespace E3_Code
+{
+    public class FakeAppUserRepository : IAppUserRepository
+    {
+        private static Dictionary<string, AppUser> AppUsers;
+        public FakeAppUserRepository()
+        {
+            if (AppUsers == null)
+            {
+                AppUsers = new Dictionary<string, AppUser>();
+                // Add some default employees to work with
+                AppUsers.Add(davebish, new AppUser
+                {
+                    UserName = davebish,
+                    Password = SocialLoafing,
+                    LastName = "Bishop",
+                    FirstName = "Barb",
+                    EmailAddress = davebish@trojans.dsu.edu
+                    IsAuthenticated = true
+
+                });
+                AppUsers.Add(moliver, new AppUser
+
+                {
+                    UserName = moliver,
+                    Password = SleepAllDayPlease,
+                    LastName = "Oliver",
+                    FirstName = "Matthew",
+                    EmailAddress = matthew.oliver@trojans.dsu.edu,
+                    IsAuthenticated = true
+
+                });
+        }
+            private string GetNextUserName()
+            {
+                int curMaxUserName = 0;
+
+                foreach (KeyValuePair<int, AppUser> keyValuePair in AppUsers)
+                {
+                    if (keyValuePair.Key > curMaxUserName)
+                    {
+                        curMaxUserName = keyValuePair.Key;
+                    }
+                }
+                return ++curMaxUserName;
+            }
+            public bool Login(string UserName, string Password)
+            {
+                string UserName = GetNextUserName();
+                appUser.UserName = UserName;
+                AppUsers.Add(appUser.Username, appUser);
+                return UserName;
+            }
+            public List<AppUser> GetAll()
+            {
+                List<AppUser> users = new List<AppUser>();
+                foreach (KeyValuePair<string, AppUser> appuser in AppUsers)
+                {
+                    users.Add(appuser.Value);
+                }
+                return users;
+            }
+            public bool SetAuthentication(string UserName, bool IsAuthenticated)
+            {
+                bool authentication = false;
+                AppUser appuser;
+                if (AppUsers.TryGetValue(UserName, out appuser))
+                {
+                    authentication = appuser.IsAuthenticated;
+                }
+                return authentication;
+            }
+            public AppUser GetByUserName(string UserName)
+            {
+            AppUsers appuser;
+            bool ignore = AppUsers.TryGetValue(UserName, out appuser);
+            return appuser;
+        }
+    }
+}
---
+++ P3 Code/P3 Code/IAppUserRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P3_Code
+{
+    public interface IAppUserRepository
+    {
+        List<AppUser> GetAll();
+        AppUser GetByUserName(string UserName);
+        bool Login(string UserName, string Password);
+        bool SetAuthentication(string UserName, bool IsAuthenticated);
+    }
+}
