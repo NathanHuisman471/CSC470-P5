018b69e5ee3629b1d04ac0581e58bb02c447f98a
matthew-oliver777 <matthew.oliver@trojans.dsu.edu>
2020-10-06 23:58:41 -0500

    Created FormSelectProject, added List Box, and added Cancel button and Select Project button.

---
+++ P3 Code/P3 Code/FormSelectProject.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace P3_Code
+{
+    partial class FormSelectProject
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.buttonCancel = new System.Windows.Forms.Button();
+            this.buttonSelectProject = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(32, 39);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(484, 251);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // buttonCancel
+            // 
+            this.buttonCancel.Location = new System.Drawing.Point(241, 331);
+            this.buttonCancel.Name = "buttonCancel";
+            this.buttonCancel.Size = new System.Drawing.Size(126, 30);
+            this.buttonCancel.TabIndex = 1;
+            this.buttonCancel.Text = "Cancel";
+            this.buttonCancel.UseVisualStyleBackColor = true;
+            this.buttonCancel.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // buttonSelectProject
+            // 
+            this.buttonSelectProject.Location = new System.Drawing.Point(392, 331);
+            this.buttonSelectProject.Name = "buttonSelectProject";
+            this.buttonSelectProject.Size = new System.Drawing.Size(124, 30);
+            this.buttonSelectProject.TabIndex = 2;
+            this.buttonSelectProject.Text = "Select Project";
+            this.buttonSelectProject.UseVisualStyleBackColor = true;
+            // 
+            // FormSelectProject
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(547, 381);
+            this.Controls.Add(this.buttonSelectProject);
+            this.Controls.Add(this.buttonCancel);
+            this.Controls.Add(this.listBox1);
+            this.Name = "FormSelectProject";
+            this.Text = "Select Project";
+            this.Load += new System.EventHandler(this.FormSelectProject_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button buttonCancel;
+        private System.Windows.Forms.Button buttonSelectProject;
+    }
+}
---
+++ P3 Code/P3 Code/FormSelectProject.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace P3_Code
+{
+    public partial class FormSelectProject : Form
+    {
+        public FormSelectProject()
+        {
+            InitializeComponent();
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void FormSelectProject_Load(object sender, EventArgs e)
+        {
+            CenterToScreen();
+        }
+    }
+}
